39d6cf3732ec0dd28fd0fdcee6c9406145c4b828
alexhuot <alexhuot@gmail.com>
2025-05-18 12:12:42 +0200

    Merge conflict resolution

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-list-app/',
+  build: {
+    outDir: 'dist'
+  },
   server: {
     hmr: {
       overlay: false
